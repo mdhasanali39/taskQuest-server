409244b6d6113f337307f053fb6677a6da8f38fc
mdhasanali39 <contact.hasankhanyt@gmail.com>
2024-03-31 23:44:09 +0600

    added another api end point for get user specific task, and added two middlewars cors for cross origin policy and express.json for req body parse to json

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-import express from 'express';
-import cors from 'cors'
-import 'dotenv/config'
-import { MongoClient, ServerApiVersion } from 'mongodb';
-
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-// mongodb uri string 
+// mongodb uri string
 const uri = process.env.MONGODB_URI_STRING;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,31 +29,61 @@ async function run() {
 
     const taskCollection = client.db("taskQuestDB").collection("tasks");
 
-    // get methods / api end points 
-    
-    // post methods / api end points 
-    app.post("/task-quest/create-task/:email", async(req,res)=>{
-        try {
-            const userEmail = req.params.email;
-            const taskInfo = req.body;
-            const acknowledge = await taskCollection.insertOne(taskInfo)
-            res.status(201).json({
-                status:true,
-                message:'Task inserted successfully',
-                acknowledge
-            })
-        } catch (err) {
-            res.status(500).json({
-                status:false,
-                message:'Internal server error', err,
-            })
-        }
+    // get methods / api end points
 
-    })
+    // get all tasks - user specific
+    app.get("/task-quest/get-all/:email", async (req, res) => {
+      try {
+        const userEmail = req.params.email;
+        const tasks = await taskCollection.find({ userEmail: userEmail }).toArray();
+        
+        // lets separate task in todo, ongoing, and completed 
+        let todo = [],ongoing = [],completed = [];
+        tasks.forEach(task =>{
+          if(task?.status === "todo"){
+            todo.push(task)
+          }else if(task?.status === "ongoing"){
+            ongoing.push(task)
+          }else{
+            completed.push(task)
+          }
+        })
+        res.status(200).json({
+          status: true,
+          message: "Task gotten successfully",
+          tasks:{todo,ongoing,completed},
+        });
+      } catch (err) {
+        res.status(500).json({
+          status: false,
+          message: `Internal server error ${err.message}`,
+        });
+      }
+    });
+
+    // post methods / api end points
+    app.post("/task-quest/create-task", async (req, res) => {
+      try {
+        const taskInfo = req.body;
+        const acknowledge = await taskCollection.insertOne(taskInfo);
+        res.status(201).json({
+          status: true,
+          message: "Task inserted successfully",
+          acknowledge,
+        });
+      } catch (err) {
+        res.status(500).json({
+          status: false,
+          message: `Internal server error ${err.message}`,
+        });
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -63,12 +91,11 @@ async function run() {
 }
 run().catch(console.dir);
 
+// test api end point
+app.get("/", (req, res) => {
+  res.send("taskQuest server running well");
+});
 
-// test api end point 
-app.get('/', (req,res)=>{
-    res.send('taskQuest server running well')
-})
-
-app.listen(port, ()=>{
-    console.log(`taskQuest server running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`taskQuest server running on port ${port}`);
+});
